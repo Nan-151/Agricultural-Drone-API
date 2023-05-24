3d75fec1fa1ca7cb7c9fa8c6ba73ba7d5d3ddc87
THAVROTH.CHEA <THAVROTH.CHEA@PNC.LAN>
2023-05-24 13:45:35 +0700

    Create Request Authentication file

---
+++ Agricultural-Drone-API-Team-7/app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthenticationController extends Controller
+{
+    
+    public function register(Request $request)
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function login()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function logout()
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
